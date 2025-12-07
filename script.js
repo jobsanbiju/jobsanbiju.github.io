3eabbeee950863a1e07571ef663ba7f98aee1bf6
jobsanbiju <jobsanbiju4@gmail.com>
2025-12-07 09:08:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const logo = document.getElementById("logo");
 const contactBtn = document.querySelector(".contact-btn");
 const contactSection = document.getElementById("contact");
 
-// HEADER SCROLL ANIMATION
 window.addEventListener("scroll", () => {
     if (window.scrollY > 70) {
         header.classList.add("scrolled");
@@ -13,11 +12,14 @@ window.addEventListener("scroll", () => {
     }
 });
 
-// CONTACT BUTTON → SCROLL TO CONTACT
 contactBtn.addEventListener("click", () => {
     contactSection.scrollIntoView({ behavior: "smooth" });
 });
 
-// CLICK LOGO / TITLE → SCROLL TO TOP
-logo.addEventListener("click", () => window.scrollTo({ top: 0, behavior: "smooth" }));
-headerTitle.addEventListener("click", () => window.scrollTo({ top: 0, behavior: "smooth" }));
+logo.addEventListener("click", () =>
+    window.scrollTo({ top: 0, behavior: "smooth" })
+);
+
+headerTitle.addEventListener("click", () =>
+    window.scrollTo({ top: 0, behavior: "smooth" })
+);
